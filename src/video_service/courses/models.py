8b453a70ff05b63776bb11c9b40a449b6c55105a
rhapsody <richer6605@gmail.com>
2018-09-22 12:14:08 +0800

    Create courses app and models
        1. Initialize a new app, courses
        2. Add two new models to courses: Course and Lesson
        3. Register models in `admin.py`

---
+++ src/video_service/courses/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+from memberships.models import Membership
+
+# Create your models here.
+class Course(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    allowed_memberships = models.ManyToManyField(Membership)
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=120)
+    course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    position = models.IntegerField()
+    ## This is used in the tutorial instead
+    # video_url = models.CharField(max_length=200)
+    video_url = models.URLField(max_length=200)
+    thumbnail = models.ImageField()
+
+    def __str__(self):
+        return self.title
